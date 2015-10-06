ba9a10c460e1e4d6da4fb342aa6aaa76c4ccabda
Pardeep Kumar <pardeepkumar@macbook-air.local>
2015-10-06 20:34:35 +0530

    Initial Commit

---
+++ Swift-Concept/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Concept
+//
+//  Created by Pardeep Kumar on 06/10/2015.
+//  Copyright © 2015 Debut Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
