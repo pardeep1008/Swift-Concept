38dd7b773b307a1157a9e66879be99a6f3b22b42
pardeep1008 <pardeep1008@gmail.com>
2015-10-21 09:02:27 +0530

    Swift Concept
    Swift concept projects….

--- Swift-Concept/ViewController.swift
+++ Swift-Concept/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Resolution
+{
+    var width = 0
+    var height = 0
 
-    override func viewDidLoad() {
+    init(width: Int, height: Int)
+    {
+        self.width = width
+        self.height = height
+    }
+}
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        let hd = Resolution(width: 640, height: 1136)
+        
+        let normal = hd
+        normal.width = 320
+        normal.height = 568
+        
+        print("Hd width is: \(hd.width) and height is: \(hd.height)")
+        print("Normal width is: \(normal.width) and height is: \(normal.height)")
+    }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
